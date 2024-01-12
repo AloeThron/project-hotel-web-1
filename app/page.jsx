afd34fb75b1b1e41d546d0c62d6aebb3a34e53df
AloeThron <lxe.tr.deat@gmail.com>
2024-01-12 08:34:06 +0700

    fix file

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,20 @@
-import Hero from '@/components/hero'
-import Footer from '@/components/footer'
-import Header from '@/components/header'
-import About from '@/components/about'
-import Featured from '@/components/featured'
+import Hero from "@/components/hero";
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import About from "@/components/about";
+import Featured from "@/components/featured";
+import Recommend from "@/components/recommend";
 
 export default function Home() {
   return (
-    <div className='max-w-[1440px] mx-auto bg-white overflow-hidden'>
+    <div className="max-w-[1440px] mx-auto bg-white overflow-hidden">
       <Header />
       <Hero />
       <Featured />
       <About />
-      <Footer />
-      <div className='h-[4000px]'></div>
+      <Recommend />
+      {/* <Footer /> */}
+      <div className="h-[4000px]"></div>
     </div>
-  )
+  );
 }
--- components/recommend.jsx
+++ components/recommend.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import Image from "next/image";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+
+import { Button } from "./ui/button";
+
+const recommendData = [
+  {
+    name: "Shanghai Hotel",
+    image: "/recommendation/hotel-1.png",
+    location: "Shanghai, China",
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae quasi mollitia enim quaerat eius ipsam ducimus recusandae, nisi delectus possimus commodi et explicabo hic.",
+    price: 40,
+    testimonial: {
+      message: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      personName: "Robert Rene",
+    },
+  },
+  {
+    name: "Peak Lodge",
+    image: "/recommendation/hotel-1.png",
+    location: "Aspen, USA",
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae quasi mollitia enim quaerat eius ipsam ducimus recusandae, nisi delectus possimus commodi et explicabo hic.",
+    price: 80,
+    testimonial: {
+      message: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      personName: "Emily Jhonson",
+    },
+  },
+  {
+    name: "Tropical Oasis Resort",
+    image: "/recommendation/hotel-1.png",
+    location: "Bali, Indonesia",
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae quasi mollitia enim quaerat eius ipsam ducimus recusandae, nisi delectus possimus commodi et explicabo hic.",
+    price: 120,
+    testimonial: {
+      message: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      personName: "Micheal Anderson",
+    },
+  },
+];
 
 export default function Recommend() {
-  return (
-    <div>Recommend</div>
-  )
+  return <div>Recommend</div>;
 }
